bb945210147a08af293d0acfde14f78ce2dd66c9
Milan Fatschel <31837228+MilanFatschel@users.noreply.github.com>
2019-07-31 21:52:50 -0400

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int pivot; 
+        int left = 0; 
+        int right = nums.size() - 1;
+        while(left <= right)
+        {
+            pivot = (left + right) / 2;
+            if(nums[pivot] == target) return pivot;
+            else {
+                if(target < nums[pivot]) right = pivot - 1;
+                else left = pivot + 1;
+            }
+        }
+        return -1;
+    }
+};
